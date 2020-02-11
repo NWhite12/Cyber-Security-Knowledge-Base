b297fd8c8b25298cb9f9f0ec058dcdc91e841287
maidel2018 <maidelfletes@gmail.com>
2020-02-11 15:51:31 -0500

    Question and Answer Forum

--- Cyber_Security_KBMS/Cyber_Security_KBMS/urls.py
+++ Cyber_Security_KBMS/Cyber_Security_KBMS/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+#from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#Specifying the path to the homepage so django knows where to redirect the user who seeks to use the homepage
+#('homepage.urls') - references the urls.py file in the homepage application
+
+    url(r'^', include ('QuestionandAnswerForum.urls')),
 ]
